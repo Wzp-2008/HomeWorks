f9bb2e57f935b48d75affcef158db57b61f57613
Wzp-2008 <minecraftwzpmc@163.com>
2023-12-26 17:53:38 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import {createRouter, createWebHashHistory} from "vue-router";
+import { createApp } from "vue";
+import "element-plus/es/components/message/style/css";
+import App from "./App.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const app = createApp(App);
 const routes = [
-    { path: '/', component: import("./views/MainPage/index.vue") },
-    { path: '/test', component: import("./views/Test/index.vue") },
-]
+	{ path: "/", component: import("./views/MainPage/index.vue") },
+	{ path: "/test", component: import("./views/Test/index.vue") },
+];
 const router = createRouter({
-    // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-    history: createWebHashHistory(),
-    routes, // `routes: routes` 的缩写
-})
-app.use(router)
-app.mount('#app');
+	// 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+	history: createWebHashHistory(),
+	routes, // `routes: routes` 的缩写
+});
+app.use(router);
+app.mount("#app");
