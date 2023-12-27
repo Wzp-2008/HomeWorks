0c70b4b651d5b19ff686c1920429e949bda2c0f2
MoYi <1545929126@qq.com>
2023-12-27 23:11:36 +0800

    [🚧修复] : 修复路由路径指定

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import "element-plus/es/components/message/style/css";
 import App from "./App.vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 import { createPinia } from "pinia";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 
 const app = createApp(App);
 const routes = [
@@ -14,6 +16,10 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.use(router);
 app.use(router);
 app.use(createPinia());
 app.mount("#app");
